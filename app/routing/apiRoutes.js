93074430a5666633dfd0b76357aeb7455e90da48
Charlotte Asencio <charlotte.e.asencio@gmail.com>
2018-06-17 13:40:10 -0400

    console logging friends information

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,15 +6,36 @@ module.exports = (function(app){
         res.json(friends);
     });
 
-    app.post("/api/friends", function(){
+    app.post("/api/friends", function(req, res){
 
-        //calculate friend compatability here 
+        var userAnswer = req.body;
+        var userScores = userAnswer.scores;
         
-    });
+        var friendMatch = '';
+		//var matchImage = '';
+        var total = 500;
 
+        for (var i = 0; i < friends.length; i++) {
+            var difference = 0;
+			for (var j = 0; j < userScores.length; j++) {
+				difference += Math.abs(friends[i].scores[j] - userScores[j]);
+        }
+
+        if (difference < total) {
+            //console.log('Closest match found = ' + difference);
+		    //console.log('Friend name = ' + friends[i].name);
+            // console.log('Friend image = ' + friends[i].photo);
+            
+            totalDifference = diff;
+            matchName = friends[i].name;
+            //matchImage = friends[i].photo;
+        }
+    }
     // Add new user
-	//friends.push(userInput);
+	friends.push(userAnswer);
 
 	// Give Response
-	//res.json({status: 'OK', matchName: matchName, matchImage: matchImage});
+res.json({status: 'OK', friendMatch: friendMatch});
+
+    });
 });
